d5c802618b9101773e707577251f7d0dbc0f4b0d
alextsybin <alexander.tsyin@gmail.com>
2021-02-22 16:47:39 +0300

    Add/Edit Logic

--- app/src/main/java/com/alextsy/todolist/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/alextsy/todolist/ui/tasks/TasksViewModel.kt
@@ -7,6 +7,8 @@ import com.alextsy.todolist.data.PreferencesManager
 import com.alextsy.todolist.data.SortOrder
 import com.alextsy.todolist.data.Task
 import com.alextsy.todolist.data.TaskDao
+import com.alextsy.todolist.ui.ADD_TASK_RESULT_OK
+import com.alextsy.todolist.ui.EDIT_TASK_RESULT_OK
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
@@ -67,9 +69,21 @@ class TasksViewModel @ViewModelInject constructor(
         tasksEventChannel.send(TasksEvent.NavigateToAddTaskScreen)
     }
 
+    fun onAddEditResult(result: Int) {
+        when (result) {
+            ADD_TASK_RESULT_OK -> showTaskSavedCOnfirmationMessage("Task added")
+            EDIT_TASK_RESULT_OK -> showTaskSavedCOnfirmationMessage("Task updated")
+        }
+    }
+
+    private fun showTaskSavedCOnfirmationMessage(text: String) = viewModelScope.launch {
+        tasksEventChannel.send(TasksEvent.ShowTaskSavedConfirmationMessage(text))
+    }
+
     sealed class TasksEvent {
         object NavigateToAddTaskScreen : TasksEvent()
         data class NavigateToEditTaskScreen(val task: Task) : TasksEvent()
         data class ShowUndoDeleteTaskMessage(val task: Task) : TasksEvent()
+        data class ShowTaskSavedConfirmationMessage(val msg: String) : TasksEvent()
     }
 }
